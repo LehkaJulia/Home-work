d3262b6a60e022b251704355864cd37357cedfb4
LehkaJulia <julialehka87@gmail.com>
2022-07-12 13:33:44 +0300

    Task 2

---
+++ HW 2/JS/script.js
@@ -0,0 +1,7 @@
+let year = prompt('Year of birthday?');
+let name = prompt('Name?');
+let lastName = prompt('Last name?');
+
+const nowYear = Number(2022);
+
+console.log (name, lastName, nowYear - year );
