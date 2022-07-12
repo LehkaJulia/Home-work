92b09e07af0dd8b45210820e07a4f9e98ced2dbc
LehkaJulia <julialehka87@gmail.com>
2022-07-12 14:10:11 +0300

    Change

---
+++ HW 3/JS/my.js
@@ -0,0 +1,5 @@
+let name = prompt('Name?');
+let lastName = prompt('Last name?');
+let day = prompt('Day of birthday?');
+let month = prompt('Month of birthday?');
+let year = prompt('Year of birthday?');
