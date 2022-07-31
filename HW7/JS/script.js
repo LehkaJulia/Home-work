ed1e8e04a39ad3183de620c707265d793907f65c
LehkaJulia <julialehka87@gmail.com>
2022-07-31 17:42:34 +0300

    HW8

--- HW7/JS/script.js
+++ HW7/JS/script.js
@@ -25,3 +25,18 @@ for (let i = 0; i < arr.length; ++i) {
 }
 
 console.log(multiplication);
+
+//let arr = [], arrLng, mul = 1;
+
+//do {
+ //   arrLng = +prompt(`Please, enter array lenght`);
+ //   if(arrLng) arrLng = Math.round(Math.abs(arrLng));
+//} while (!arrLng || arrLng > 10 || arrLng < 2)
+
+//for(let i = 0; i < arrLng; i++) {
+ //   arr[i] = Math.round(Math.random()*10); 
+ //   mul *= arr[i];
+//}
+
+//console.log(arr);
+//console.log(mul);
---
+++ HW8/HW8.1/JS/script.js
@@ -0,0 +1,26 @@
+let hero = ['Ivan'];
+let native = ['York','Of'];
+let destination = ['Poltava','In'];
+let rainbow = hero.concat(native,destination).reverse()
+
+rainbow.splice(0,2,`Richard`)
+rainbow.splice(3,1,'Gave','Battle','In','Vain')
+
+let color = [`red`, `orange` ,  `yellow`, `green`, `light`, `blue`, `violet`]
+
+let final=[]
+
+for (i=0; i < rainbow.length; i++) {
+    final.push(`
+    <div class="wrapper">
+    <div class="circle ${color[i]}" > </div>
+    <p>${rainbow[i]}</p>
+    </div>`)   
+}
+document.write(
+    `<div> ${final.join('')}</div>
+`)
+
+
+
+
