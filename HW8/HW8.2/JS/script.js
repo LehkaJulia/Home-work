a5fa12200e094363b859072a2a83f2e0dc483650
LehkaJulia <julialehka87@gmail.com>
2022-08-01 11:49:16 +0300

    HW8

--- HW8/HW8.2/JS/script.js
+++ HW8/HW8.2/JS/script.js
@@ -12,21 +12,11 @@ const sports = [
 	['bicyclist','🚴‍']
 ];
 
-
-console.log(sports)
 let winter_sports = sports.slice(0,2).concat(sports.slice(3,5))
-console.log(winter_sports)
-
-let winte_array
-let winter
-
 
 let summer_sports = sports.slice(5,7).concat(sports.slice(9))
-console.log(summer_sports)
 
 let fruits = sports.slice(2,3).concat(sports.slice(7,9))
-
-console.log(fruits)
 
 document.write(`<ul>
 <li>***Winter sports***</li>
