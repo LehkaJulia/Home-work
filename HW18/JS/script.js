9bcc475cc3db8ea98ce87ba1a3e2eb6809bba0e5
LehkaJulia <julialehka87@gmail.com>
2022-09-01 01:28:20 +0300

    HW18

--- HW18/JS/script.js
+++ HW18/JS/script.js
@@ -1,22 +1,25 @@
 const body = document.querySelector(`body`);
 const myDiv = document.querySelector(`.block`);
-let height = body.clientHeight-myDiv.clientHeight;
-let width = body.clientWidth-myDiv.clientWidth;
 myDiv.style.left = 0;
 myDiv.style.top = 0;
 myDiv.style.backgroundColor = `#000000`;
 
+// console.log(height)
+// console.log(width)
 const setBgDiv = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     myDiv.style.backgroundColor = "#" + randomColor; 
 }
 
 function getRandomHight() {
-    return Math.round(Math.random()*height);
+    let height = body.clientHeight-myDiv.clientHeight;
+    return Math.round(Math.random()*(body.clientHeight-myDiv.clientHeight));
 }
 
 function getRandomWidth() {
-    return Math.round(Math.random()*width);
+    let width = body.clientWidth-myDiv.clientWidth;
+    console.log(width)
+    return Math.round(Math.random()*(body.clientWidth-myDiv.clientWidth));
 }
 const coloringBlock = setInterval(setBgDiv, 500);
 const movingBlock = setInterval(() => {
